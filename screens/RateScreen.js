437efb5c53b713ee7beaf2474cb3d0bdfbbb187f
Jorge Suarez <jorgsuau@live.com.co>
2019-04-16 11:31:32 -0500

    Rates section okay. Needs styles and complete data

--- screens/RateScreen.js
+++ screens/RateScreen.js
@@ -1,146 +1,145 @@
-import React from 'react';
-import { AppRegistry, ActivityIndicator, View, ScrollView, Text, StyleSheet, TextInput, Button, Alert, TouchableOpacity, ImageBackground, FlatList } from 'react-native';
-import SearchInput, { createFilter } from 'react-native-search-filter';
+import React, { Component } from 'react';
+import { View, Text, FlatList, ActivityIndicator, StyleSheet, ScrollView } from 'react-native';
+import { ListItem, SearchBar } from 'react-native-elements';
 
-
-
-export default class RateScreen extends React.Component {
-  static navigationOptions = {
-    header: null,
-  };
-
+class FlatListDemo extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      titleText: "TASAS DE INTERÉS ACTIVAS CRÉDITO DE VIVIENDA",
-      searchTerm: '',
-      isLoading: true, // check if json data (online) is fetching
-      dataSource: [], // store an object of json data
-      dataSourceComplete: [],
-      error: null,    
+
+    this.state = {
+      loading: true,
+      data: [],
+      error: null,
     };
-    this.dataSource = [];
-    this.searchFilterFunction = this.searchFilterFunction.bind(this);
+
+    this.arrayholder = [];
   }
-
-
-
-  searchFilterFunction = text => {    
-    const CompleteData = this.state.dataSourceComplete;
-    const newData = this.state.dataSource.filter(item => {      
-      const itemData = `${item.sigla.toUpperCase()}`;
-      const itemDataComplete = this.state.dataSourceComplete;
-       const textData = text.toUpperCase();
-       const exist = itemData.indexOf(textData); 
-      return itemData.indexOf(textData) > -1; 
-      
-       
-         
-    });    
-    //this.setState({ dataSource: newData }); 
-    this.setState({
-      isLoading: false, // already no loading
-      dataSource: newData
-    });
-    //console.log(this.state.dataSource);
-     
-  }; 
 
   componentDidMount() {
-   
-    return fetch("https://www.datos.gov.co/resource/kq52-uq9b.json")
-          .then((response) => response.json())
-          .then((responseJson) => {
-            // set state value
-            this.setState({
-              isLoading: false, // already loading
-              dataSource: responseJson,
-              dataSourceComplete: responseJson,
-            });
-
-            
-            
-          })
-          .catch((error) => {
-            console.warn(error);
-          });
-
-          //this.searchFilterFunction();
+    this.makeRemoteRequest();
   }
 
-  render() {
+  makeRemoteRequest = () => {
+    const url = `https://www.datos.gov.co/resource/kq52-uq9b.json`;
+    this.setState({ loading: true });
+
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          data: res,
+          error: res.error || null,
+          loading: false,
+        });
+        this.arrayholder = res;
+      })
+      .catch(error => {
+        this.setState({ error, loading: false });
+      });
+  };
 
 
-  if(this.state.isLoading) {
-    return(
-      <View style={{flex: 1, padding: 20, color: '#ffffff', backgroundColor: '#333333'}}>
-        <ActivityIndicator/>
-      </View>
-    )
-  }
-     
+
+  searchFilterFunction = text => {
+    this.setState({
+      value: text,
+    });
+
+    const newData = this.arrayholder.filter(item => {
+      const itemData = `${item.sigla.toUpperCase()}`;
+      const textData = text.toUpperCase();
+
+      return itemData.indexOf(textData) > -1;
+    });
+    this.setState({
+      data: newData,
+    });
+  };
+
+  renderHeader = () => {
     return (
-
-      <View style={styles.container}>
-        <Text style={styles.bigTitle}>
-          {this.state.titleText}
-        </Text>
-
-        <Text style={styles.smallTitle}>
-          Son las tasas de interés activas cobradas por los establecimientos de crédito en los préstamos de vivienda. 
-        </Text>
-
-        <Text style={styles.tinyTitle}>
-          La información se actualiza de Datos Abiertos Colombia.
-        </Text>
-
-        <SearchInput 
-          onChangeText={text => this.searchFilterFunction(text)}
-          style={styles.searchInput}
-          placeholder="Escriba una palabra clave a buscar"
-        />
-
-        <ScrollView style={styles.container}>
-        <View style={styles.containerInner}>
-
-
-
-          <FlatList
-          data={this.state.dataSource}
-          renderItem={({item}) => {
-            return (
-              <View>
-                <Text style={styles.info}>{item.sigla} is {item.modalidad_de_credito} years old. {item.tipo_entidad == "male" ? "He" : "She"} likes {item.valor}</Text>
-              </View>
-            )
-          }}
-          keyExtractor={(item, index) => index.toString()}
-          />
-
+      <SearchBar
+        placeholder="Type Here..."
+        lightTheme
+        round
+        onChangeText={text => this.searchFilterFunction(text)}
+        autoCorrect={false}
+        value={this.state.value}
+      />
+    );
+  };
 
  
 
-     
+  render() {
+    if (this.state.loading) {
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
+   
+    return (
+
+      <View style={styles.container}>
+        <SearchBar
+        placeholder="Escriba la entidad bancaria..."
+        darkTheme
+        round
+        onChangeText={text => this.searchFilterFunction(text)}
+        autoCorrect={false}
+        value={this.state.value}
+      />
+        <ScrollView>
+        <View style={styles.containerInner}>
+        <FlatList
+          data={this.state.data}
+          renderItem={({ item }) => {
+            if(item.modalidad_de_credito == "CREDITO VIVIENDA"){
+              return (
+                <View style={styles.boxStyle}>
+                  <Text style={styles.commissionStyle}>{item.sigla} is {item.modalidad_de_credito} years old. {item.tipo_entidad} likes {item.valor}</Text>
+                </View>
+                )
+            }
+            } 
+        }
+          keyExtractor={(item, index) => index.toString()}
+          //ListHeaderComponent={this.renderHeader}
+        />
+        </View>
+        </ScrollView>
+      </View>
       
-          </View>
-        </ScrollView>               
-      </View> 
+      
     );
   }
 }
 
+export default FlatListDemo;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 10,
-    paddingBottom: 20,
-    paddingLeft: 20,
-    paddingRight: 20,
+    paddingLeft: 0,
+    paddingRight: 0,
     backgroundColor: '#282B33',
     color: '#ffffff',
   },
   containerInner: {
-    paddingBottom: 40
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: 10,
+    paddingBottom: 20,
+  },
+  boxStyle: {
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    backgroundColor: "#353A45",
+    borderRadius: 6,
+    marginBottom: 6,
   },
   bigTitle: {
     textAlign: 'center',
@@ -149,21 +148,14 @@ const styles = StyleSheet.create({
     textTransform: 'uppercase',
     fontFamily: "Roboto-Bold",
   },
-  bigTitleChapter: {
+  bigTitleSpecial: {
     textAlign: 'center',
-    fontSize: 20,
+    fontSize: 16,
     color: '#ffffff',
     textTransform: 'uppercase',
     fontFamily: "Roboto-Bold",
-    paddingTop: 20,
-  },
-   bigSubTitleChapter: {
-    textAlign: 'center',
-    fontSize: 20,
-    color: '#ffffff',
-    textTransform: 'uppercase',
-    fontFamily: "Roboto-Bold",
-    marginBottom:25,
+    marginTop: 10,
+    marginBottom: 0,
   },
   smallTitle: {
     textAlign: 'center',
@@ -173,23 +165,13 @@ const styles = StyleSheet.create({
     marginBottom: 6,
     fontFamily: "Roboto-Thin",
   },
-  info: {
-    textAlign: 'justify',
-    fontSize: 14,
-    color: '#ffffff',
-  },
-  emailItem:{
-    paddingTop: 10,
-    paddingBottom: 5,
-    color: 'rgba(255,255,255,1)',
-    fontFamily: "Roboto-Bold",
-    fontSize: 16,
-  },
-  emailSubject: {
-    color: '#ffffff',
+  smallTitleXS: {
+    textAlign: 'center',
+    fontSize: 13,
+    color: '#cccccc',
+    marginTop: 6,
+    marginBottom: 6,
     fontFamily: "Roboto-Thin",
-    fontSize: 15,
-    textAlign: 'justify',
   },
   searchInput:{
     padding: 10,
@@ -197,7 +179,8 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     fontFamily: "Roboto-Thin",
     textAlign: 'center',
-    marginTop: 15,
+    marginTop: 5,
+    marginBottom: 20,
     fontSize: 15,
     color: '#ffffff',
     borderRadius: 20,
@@ -208,23 +191,186 @@ const styles = StyleSheet.create({
     elevation: 1,
     backgroundColor: '#353A45',
   },
-  textNotes: {
-    color: 'rgba(255,255,255,.5)',
-    marginBottom:5,
-    marginTop:5,
-    fontSize: 10,
+  info: {
+    textAlign: 'justify',
+    fontSize: 16,
+  },
+  commissionStyle: {
+    flex: 1, 
+    flexDirection: 'row',
+    color: "#ffffff",
+  },
+  iconRte: {
+    width: 30,
+    height: 30,
+    borderRadius: 50,
+    backgroundColor: '#282B33',
+    borderColor: '#004d4d',
+    borderWidth: 3,
+    borderStyle: 'dashed',
+    marginTop: 5,
+    marginRight: 15,
+    marginBottom:10,
+    marginLeft: 15,
+  },
+  iconNotaryfees: {
+    width: 30,
+    height: 30,
+    borderRadius: 50,
+    backgroundColor: '#282B33',
+    borderColor: '#008080',
+    borderWidth: 3,
+    borderStyle: 'dashed',
+    marginTop: 5,
+    marginRight: 15,
+    marginBottom:10,
+    marginLeft: 15,
+  },
+  iconNotaryTaxes: {
+    width: 30,
+    height: 30,
+    borderRadius: 50,
+    backgroundColor: '#282B33',
+    borderColor: '#00b3b3',
+    borderWidth: 3,
+    borderStyle: 'dashed',
+    marginTop: 5,
+    marginRight: 15,
+    marginBottom:10,
+    marginLeft: 15,
+  },
+  iconCopies: {
+    width: 30,
+    height: 30,
+    borderRadius: 50,
+    backgroundColor: '#282B33',
+    borderColor: '#4B8893',
+    borderWidth: 3,
+    borderStyle: 'dashed',
+    marginTop: 5,
+    marginRight: 15,
+    marginBottom:10,
+    marginLeft: 15,
+  },
+  iconCharity: {
+    width: 30,
+    height: 30,
+    borderRadius: 50,
+    backgroundColor: '#282B33',
+    borderColor: '#6EA0A9',
+    borderWidth: 3,
+    borderStyle: 'dashed',
+    marginTop: 5,
+    marginRight: 15,
+    marginBottom:10,
+    marginLeft: 15,
+  },
+  iconRegisterTaxes: {
+    width: 30,
+    height: 30,
+    borderRadius: 50,
+    backgroundColor: '#282B33',
+    borderColor: '#93B8BF',
+    borderWidth: 3,
+    borderStyle: 'dashed',
+    marginTop: 5,
+    marginRight: 15,
+    marginBottom:10,
+    marginLeft: 15,
+  },
+  iconCertificates: {
+    width: 30,
+    height: 30,
+    borderRadius: 50,
+    backgroundColor: '#282B33',
+    borderColor: '#B7D0D3',
+    borderWidth: 3,
+    borderStyle: 'dashed',
+    marginTop: 5,
+    marginRight: 15,
+    marginBottom:10,
+    marginLeft: 15,
+  },
+  iconTotals: {
+    width: 30,
+    height: 30,
+    borderRadius: 50,
+    backgroundColor: '#282B33',
+    borderColor: 'rgb(254, 160, 79)',
+    borderWidth: 3,
+    borderStyle: 'dashed',
+    marginTop: 5,
+    marginRight: 15,
+    marginBottom:10,
+    marginLeft: 15,
+  },
+  commissionLabel: {
+    width: '60%',
+    fontSize: 15,
+    color: '#ffffff',
+    fontFamily: "Roboto-Bold",
+    marginTop: 8,
+  },
+  commissionLabelV: {
+    width: '30%',
+    fontSize: 15,
+    color: '#ffffff',
+    fontFamily: "Roboto-Thin",
+    marginTop: 8,
+  },
+  legends: {
+    fontSize: 15,
+    color: '#ffffff',
+    fontFamily: "Roboto-Thin",
+    position: 'relative',
+    display: 'flex'
+  
+  },
+  legendsInner: {
+    fontSize: 15,
+    color: '#ffffff',
+    fontFamily: "Roboto-Thin",
+    marginTop:300
+  },
+  legendsContainer: {
+    paddingTop:280,
+  },
+  totalStyle: {
+    flex: 1,
+    fontSize: 15,
+    color: '#ffffff',
+    fontFamily: "Roboto-Thin",
+    marginTop:-160,
+    marginBottom: 115,
+    textAlign: 'center',
+    width: '100%',
+  },
+  totalTitleStyle: {
+    fontSize: 12,
+    color: '#ffffff',
+    fontFamily: "Roboto-Thin",
+    marginTop:0,
     textAlign: 'center',
   },
-  tinyTitle: {
-    color: 'rgba(255,255,255,.5)',
-    marginBottom:5,
-    marginTop:5,
-    fontSize: 10,
+  totalValueStyle: {
+    fontSize: 12,
+    color: '#ffffff',
+    fontFamily: "Roboto-Thin",
+    marginTop:0,
     textAlign: 'center',
   },
-  wordHightLigther: {
-    backgroundColor: 'yellow',
+  totalTopay: {
+    fontSize: 16,
+    color: 'rgb(254, 160, 79)',
+    fontFamily: "Roboto-Bold",
+    marginTop:0,
+    textAlign: 'center',
+  },
+  centerLegendsData: {
+    width: '90%',
+    justifyContent:'center',
+    marginBottom:20,
   }
- 
+
 });
 
